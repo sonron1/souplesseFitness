4316e007b8d11d845d05257321df5e9268033af4
sonron1 <lolathread@gmail.com>
2025-07-21 02:08:58 +0100

    Update Controller Client, Commande Home et Produit

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,17 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
+use App\Repository\ClientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ClientController extends AbstractController
+#[Route('/client', name: 'client_')]
+class ClientController extends AbstractController
 {
-    #[Route('/client', name: 'app_client')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(ClientRepository $clientRepository): Response
     {
+        $clients = $clientRepository->findAll();
+
         return $this->render('client/index.html.twig', [
-            'controller_name' => 'ClientController',
+            'clients' => $clients,
         ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        if ($request->isMethod('POST')) {
+            $client = new Client();
+            $client->setNom($request->request->get('nom'));
+            $client->setPrenom($request->request->get('prenom'));
+            $client->setEmail($request->request->get('email'));
+            $client->setTelephone($request->request->get('telephone'));
+
+            $em->persist($client);
+            $em->flush();
+
+            return $this->redirectToRoute('client_index');
+        }
+
+        return $this->render('client/new.html.twig');
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(Client $client): Response
+    {
+        return $this->render('client/show.html.twig', [
+            'client' => $client,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Client $client, EntityManagerInterface $em): Response
+    {
+        if ($request->isMethod('POST')) {
+            $client->setNom($request->request->get('nom'));
+            $client->setPrenom($request->request->get('prenom'));
+            $client->setEmail($request->request->get('email'));
+            $client->setTelephone($request->request->get('telephone'));
+
+            $em->flush();
+
+            return $this->redirectToRoute('client_index');
+        }
+
+        return $this->render('client/edit.html.twig', [
+            'client' => $client,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'delete', methods: ['POST'])]
+    public function delete(Client $client, EntityManagerInterface $em): Response
+    {
+        $em->remove($client);
+        $em->flush();
+
+        return $this->redirectToRoute('client_index');
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
+use App\Repository\ClientRepository;
+use App\Repository\CommandeRepository;
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
-    {
+    #[Route('/', name: 'app_home')]
+    public function index(
+        ClientRepository $clientRepository,
+        ProduitRepository $produitRepository,
+        CommandeRepository $commandeRepository
+    ): Response {
+        // Statistiques pour le tableau de bord
+        $stats = [
+            'total_clients' => $clientRepository->count([]),
+            'total_produits' => $produitRepository->count([]),
+            'total_commandes' => $commandeRepository->count([]),
+            'commandes_en_attente' => $commandeRepository->count(['statut' => 'en_attente']),
+        ];
+
+        // Dernières commandes
+        $dernieresCommandes = $commandeRepository->findBy(
+            [],
+            ['dateCommande' => 'DESC'],
+            5
+        );
+
+        // Produits avec stock faible (moins de 10)
+        $produitsStockFaible = $produitRepository->createQueryBuilder('p')
+            ->where('p.stock < 10')
+            ->orderBy('p.stock', 'ASC')
+            ->setMaxResults(5)
+            ->getQuery()
+            ->getResult();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'stats' => $stats,
+            'dernieres_commandes' => $dernieresCommandes,
+            'produits_stock_faible' => $produitsStockFaible,
         ]);
     }
 }
